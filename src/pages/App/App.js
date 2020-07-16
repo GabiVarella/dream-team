2175a69576c4daacf7ae7494eac844731ba85a1a
Gabriel Varella <gabrielvarella@Gabriels-MacBook-Pro.local>
2020-07-16 09:15:00 -0700

    footer

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
 import './App.css';
 import NavBar from '../../components/NavBar/NavBar';
+import Footer from '../../components/Footer/Footer';
 import LoginPage from '../LoginPage/LoginPage';
 import SignupPage from '../SignupPage/SignupPage';
 import userService from '../../services/userService';
@@ -96,6 +97,7 @@ class App extends Component {
             history={history}
           />
         }/>
+        {/* <Footer /> */}
       </>
     );
   }
--- src/pages/LeaguePage/LeaguePage.jsx
+++ src/pages/LeaguePage/LeaguePage.jsx
@@ -11,22 +11,23 @@ const LeaguePage = () => {
             <li class="collection-item">
                 <Link
                     to='/brazil'
-                    ><h5>Brazil <span role="img">🇧🇷</span></h5>
+                    ><h5>Brazil <span role="img" aria-label="brazil-flag">🇧🇷</span></h5>
                 </Link>
             </li>
             <li class="collection-item">
                 <Link
                     to='/england'
-                    ><h5>England <span role="img">🏴󠁧󠁢󠁥󠁮󠁧󠁿</span></h5>
+                    ><h5>England <span role="img" aria-label="england-flag">🏴󠁧󠁢󠁥󠁮󠁧󠁿</span></h5>
                 </Link>
             </li>
             <li class="collection-item">
                 <Link
                     to='/spain'
-                    ><h5>Spain <span role="img">🇪🇸</span></h5>
+                    ><h5>Spain <span role="img" aria-label="spain-flag">🇪🇸</span></h5>
                 </Link>
             </li>
             
+            <li class="collection-item"><h6>More Coming Soon...</h6></li>
         </ul>
         </>
 

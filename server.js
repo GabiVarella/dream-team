9b65817663ca303e7cecee0b6aee9fe2d2b8c0b3
Gabriel Varella <gabrielvarella@Gabriels-MacBook-Pro.local>
2020-07-21 14:56:59 -0700

    redeploy 3

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const favicon = require('serve-favicon');
 const logger = require('morgan');
 const port = process.env.PORT || 3001;
 
@@ -14,9 +16,17 @@ const apiRouter = require('./routes/api-requests')
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.use('/api', apiRouter)
 app.use('/api/users', userRouter);
+
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(port, ()=> {
     console.log(`Express is listening on port ${port}.`)

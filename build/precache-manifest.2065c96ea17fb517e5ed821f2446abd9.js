31ac0a52bc8a6a7a38d403cf4765dba653aa20ee
Gabriel Varella <gabrielvarella@Gabriels-MacBook-Pro.local>
2020-07-21 15:11:47 -0700

    redeploy 4

---
+++ build/precache-manifest.2065c96ea17fb517e5ed821f2446abd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d372f1a6392292e98914a97e9c67114",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ec252e3e056e8d4b54a",
+    "url": "/static/css/main.a5c737bc.chunk.css"
+  },
+  {
+    "revision": "7b8e21afef81748ced88",
+    "url": "/static/js/2.6b1dfba1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b1dfba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec252e3e056e8d4b54a",
+    "url": "/static/js/main.d0a09ae0.chunk.js"
+  },
+  {
+    "revision": "6912a1edb0f9ef78ed97",
+    "url": "/static/js/runtime-main.11dfd0f1.js"
+  }
+]);
